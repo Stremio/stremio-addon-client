677758522245bb165a1dd9e5b7be966ec12ea115
Ivo Georgiev <ivo@linvo.org>
2018-04-08 16:13:42 +0300

    collection added

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./lib/client')
+const client = require('./lib/client')
+const collection = require('./lib/collection')
+
+module.exports = {
+	AddonClient: client.AddonClient,
+	AddonCollection: collection.AddonCollection,
+	constructFromManifest: client.constructFromManifest,
+	detectFromURL: client.detectFromURL,
+}
