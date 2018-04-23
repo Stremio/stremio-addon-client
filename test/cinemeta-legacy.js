1d8d8a487505e5ef0984ac486abd043acb69a7bf
Ivo Georgiev <ivo@strem.io>
2018-04-23 20:55:52 +0300

    cinemeta-legacy test fixed: better style, no longer requires fanart

--- test/cinemeta-legacy.js
+++ test/cinemeta-legacy.js
@@ -4,9 +4,8 @@ const AddonClient = require('../lib/client')
 
 const tape = require('tape')
 
+let addon
 tape('detectFromURL: detect and use cinemeta LEGACY URL and work', function(t) {
-	let addon
-
 	AddonClient.detectFromURL('https://cinemeta.strem.io/stremio/v1')
 	.then(function(resp) {
 		t.ok(resp, 'has response')
@@ -15,22 +14,24 @@ tape('detectFromURL: detect and use cinemeta LEGACY URL and work', function(t) {
 		t.ok(resp.addon.manifest.catalogs.length, 'has catalogs length')
 
 		addon = resp.addon
-	
-		return addon.get('catalog', 'movie', 'top')
+
+		t.end()
 	})
+})
+
+tape('can get catalog/movie/top', function(t) {
+	return addon.get('catalog', 'movie', 'top')
 	.then(function(resp) {
 		let metas = resp.metas
 		t.ok(Array.isArray(metas), 'resp is an array')
-		
-		return addon.get('meta', 'movie', 'tt0816692')
+		t.end()		
 	})
+})
+
+tape('can get meta/movie/{id}', function(t) {
+	return addon.get('meta', 'movie', 'tt0816692')
 	.then(function(resp) {
 		t.ok(resp.meta, '.meta returned')
-		t.ok(resp.meta.fanart, 'response has fanart')
-		t.end()
-	})
-	.catch(function(err) {
-		t.error(err, 'error')
 		t.end()
 	})
 })
