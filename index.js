302fd392ebb82201ba1f3e89c8f7c4abeba97a81
Ivo Georgiev <ivo@strem.io>
2018-07-24 01:50:41 +0300

    mapURL added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const client = require('./lib/client')
 const collection = require('./lib/collection')
+const mapURL = require('./lib/utils/mapURL')
 
 module.exports = {
 	AddonClient: client.AddonClient,
 	AddonCollection: collection.AddonCollection,
 	constructFromManifest: client.constructFromManifest,
 	detectFromURL: client.detectFromURL,
+	mapURL: mapURL,
 }
