206ff0e44ec6856adde64616042963f5cb40b7d3
Ivo Georgiev <ivo@strem.io>
2018-07-27 22:20:45 +0300

    expose transports

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var client = require('./lib/client')
 var collection = require('./lib/collection')
 var mapURL = require('./lib/util/mapURL')
+var transports = require('./lib/transports')
 
 module.exports = {
 	AddonClient: client.AddonClient,
 	AddonCollection: collection.AddonCollection,
 	detectFromURL: client.detectFromURL,
 	mapURL: mapURL,
+	transports: transports,
 }
