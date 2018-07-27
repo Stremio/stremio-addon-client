c94d512832d1ece91c1792b2f51b103838b46cfb
Ivo Georgiev <ivo@strem.io>
2018-07-27 22:13:16 +0300

    remove an obsolete thing we exposed

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var mapURL = require('./lib/util/mapURL')
 module.exports = {
 	AddonClient: client.AddonClient,
 	AddonCollection: collection.AddonCollection,
-	constructFromManifest: client.constructFromManifest,
 	detectFromURL: client.detectFromURL,
 	mapURL: mapURL,
 }
