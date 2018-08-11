150a43e44f878cc2f26b6c255838ad033e3017ad
Ivo Georgiev <ivo@strem.io>
2018-08-11 12:40:20 +0100

    isSupported now supports catalogs

--- test/isSupported.js
+++ test/isSupported.js
@@ -3,22 +3,21 @@ var tape = require('tape')
 var isSupported = require('../lib/util/isSupported')
 
 tape('isSupported: checks for resource and types', function(t) {
-	var m = { resources: ['catalog', 'stream'], types: ['movie', 'channel'] }
-	t.equal(isSupported(m, 'catalog', 'movie', '1'), true, 'all match')
+	var m = { resources: ['meta', 'stream'], types: ['movie', 'channel'] }
+	t.equal(isSupported(m, 'meta', 'movie', '1'), true, 'all match')
 	t.equal(isSupported(m, 'stream', 'movie', '1'), true, 'all match - second resource')
-	t.equal(isSupported(m, 'catalog', 'channel', '1'), true, 'all match - second type')
-	t.equal(isSupported(m, 'meta', 'movie', '1'), false, 'resource does not match')
-	t.equal(isSupported(m, 'catalog', 'series', '1'), false, 'type does not match')
-	t.equal(isSupported(m, 'meta', 'series', '1'), false, 'neither match')
+	t.equal(isSupported(m, 'meta', 'channel', '1'), true, 'all match - second type')
+	t.equal(isSupported(m, 'meta', 'series', '1'), false, 'type does not match')
+	t.equal(isSupported(m, 'madeup', 'series', '1'), false, 'neither match')
 	t.end()
 })
 
 tape('isSupported: checks for idPrefixes', function(t) {
-	var m = { resources: ['catalog'], types: ['movie'], idPrefixes: ['tt', 'yt_id:'] }
+	var m = { resources: ['meta'], types: ['movie'], idPrefixes: ['tt', 'yt_id:'] }
 
-	t.equal(isSupported(m, 'catalog', 'movie', '1'), false, 'does not match')
-	t.equal(isSupported(m, 'catalog', 'movie', 'tt1'), true, 'matches first prefix')
-	t.equal(isSupported(m, 'catalog', 'movie', 'yt_id:1'), true, 'matches second prefix')
+	t.equal(isSupported(m, 'meta', 'movie', '1'), false, 'does not match')
+	t.equal(isSupported(m, 'meta', 'movie', 'tt1'), true, 'matches first prefix')
+	t.equal(isSupported(m, 'meta', 'movie', 'yt_id:1'), true, 'matches second prefix')
 	t.end()
 })
 
